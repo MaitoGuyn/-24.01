f2b2d234a63ba452454185c095231cb51ad76b6e
Сафиуллин Самат <samatsafiullin7@gmail.com>
2024-01-24 10:57:19 +0300

    Добавьте файлы проекта.

---
+++ Самостоятельная/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+ static bool IsValid(string s)
+        {
+                var count = 0;
+                foreach (var c in s)
+                {
+                    if (c == '(')
+                        count++;
+
+                    if (c == ')')
+                    {
+                        if (count == 0) return false;
+                        count--;
+                    }
+                }
+                return true;
+        }
+Console.WriteLine(IsValid("((2+3)*5 - ((7-3) / 3) * 8))"));
+
